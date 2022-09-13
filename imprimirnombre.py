03e240a287a0fdb4e861817382dda72957bb3fa1
angiec7 <112184498+angiec7@users.noreply.github.com>
2022-09-12 20:57:57 -0600

    imprimir nombre

---
+++ imprimirnombre.py
@@ -0,0 +1,11 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+# Author : Angie Castellanos
+# Fecha: 2022.09.12
+# Editado : 2022.09.08
+
+from re import X
+
+
+nombre=input("¿Cómo te llamas?")
+
+print("¡Hola "+nombre+"!")
